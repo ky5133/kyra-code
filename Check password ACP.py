6239dfaa3ea739f57f6c6aac04bf2525dfb3e2dc
kyra3155 <dinshika1986@gmail.com>
2026-04-16 19:23:26 +0400

    commit

--- Check password ACP.py
+++ Check password ACP.py
@@ -1,37 +1,33 @@
 import tkinter as tk
 
-root=tk.Tk()
-root.geometry('400x400')
-root.title('Password Strenght Checker App')
+root = tk.Tk()
+root.title("Length Converter App")
+root.geometry("400x400")
 
-def check_password():
-    password=entry.get()
-    lenght=len(password)
-
-    if lenght<=5 :
-        result_label.config(root,text="Weak",fg='ed')
-
-    elif  6 <= lenght <= 8 :
-        result_label.config(root,text="Medium",fg='Yellow')
-
-    elif 8 <= lenght <= 12 :
-        result_label.config(root,text="strong",fg='Green')      
-
+def check_strength():
+    password = entry.get()
+    length = len(password)
+    
+    if length <= 5:
+        result_label.config(text="Weak", fg="red")
+    elif 6 <= length <= 8:
+        result_label.config(text="Medium", fg="yellow")
+    elif 8 < length <= 12:
+        result_label.config(text="Strong", fg="lightgreen")
     else:
-        result_label.config(root,text="Very Strong",fg="Dark Green")
+        result_label.config(text="Very Strong", fg="darkgreen")
 
 
-label=tk.Label(root,text="Enter Password:") 
-label.pack(pady=10)  
+label = tk.Label(root, text="Enter Password:")
+label.pack(pady=10)
 
-entry= tk.Entry(root,show="*")
+entry = tk.Entry(root, show="*")
 entry.pack(pady=5)
 
-result_label=tk.Label(root,text="",font=("Arial",14,"bold"))
-result_label.pack(pady=20)
-
-check_button=tk.Label(root,text="Check password",command=check_password)
+check_button = tk.Button(root, text="Check Strength", command=check_strength)
 check_button.pack(pady=10)
 
+result_label = tk.Label(root, text="", font=("Arial", 14, "bold"))
+result_label.pack(pady=20)
 
 root.mainloop()
